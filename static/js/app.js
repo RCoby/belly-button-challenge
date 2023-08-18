4da37da91fe987d0497003774b7c0c07d0cc44bf
RCoby <rachelcdrayton@gmailo.com>
2023-08-18 20:56:09 +1000

    Update file

--- static/js/app.js
+++ static/js/app.js
@@ -19,16 +19,18 @@ d3.json(url).then(function(data){
     dropdown.appendChild(option);
   });
 
-    // function to handle dropdown option change
-    dropdown.addEventListener("change", function() {
-    let selectedValue=dropdown.value;
-    console.log("Selected:", selectedValue);
-    
-     //Demographic info    
+  //initialise with first test subject data
+  let initialselectedValue = namesArray[0];
+  updateData(initialselectedValue);
+
+
+  //function to update displayed data
+  function updateData(selectedValue) {
+    //Demographic info    
     let metadataArray=data.metadata;
     let selectedMetadata=metadataArray.find(metadata =>metadata.id ==selectedValue);
     
-     //display "metadata" information 
+    //display "metadata" information 
     let Demographic=document.getElementById("sample-metadata");
     Demographic.innerHTML=`
       ID: ${selectedMetadata.id}<br>
@@ -39,13 +41,10 @@ d3.json(url).then(function(data){
       BB Type: ${selectedMetadata.bbtype}<br>
       Wash Frequency: ${selectedMetadata.wfreq}`
   
-
-  
+    // bar chart
     let sample = data.samples;
     let selectedSample = sample.find(sample => sample.id === selectedValue);
-      console.log("Selected Sample:", selectedSample);
 
-    //bar chart
     let barChartData= {
       x: selectedSample.sample_values.slice(0, 10).reverse(),
       y: selectedSample.otu_ids.map(String).slice(0, 10).map(id=>`OTU ${id}`).reverse(),
@@ -53,29 +52,40 @@ d3.json(url).then(function(data){
       type: 'bar',
       orientation: 'h'
     };
-      console.log("barChartData:", barChartData);
 
     let layout = {
-      title: `Top 10 OTUs for Sample ${selectedSample.id}`,
+      title: `Top 10 OTUs for Test Subject ${selectedSample.id}`,
       xaxis: { title: 'Sample Values' },
-      yaxis: { title: 'OTU IDs' }
     };
-      console.log("layout:", layout);
    
     Plotly.newPlot('bar', [barChartData], layout);
-    });
+    
+
+    //bubble chart 
+    let bubbleData={
+      x: selectedSample.otu_ids,
+      y: selectedSample.sample_values,
+      text: selectedSample.otu_labels,
+      mode: 'markers',
+      marker: {
+        size: selectedSample.sample_values,
+        color: selectedSample.otu_ids,
+        colorscale: 'Veridis'}
+    };
+
+    let bubble_layout = {
+      title: `Test Subject ${selectedSample.id} Samples`,
+      xaxis: {type:'scatter', title: 'OTU ID' },
+    };
+   
+    Plotly.newPlot('bubble', [bubbleData], bubble_layout);
+  }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+  // function to handle dropdown option change
+  dropdown.addEventListener("change", function () {
+    let selectedValue=dropdown.value;
+    updateData(selectedValue); 
   });
+
+});
